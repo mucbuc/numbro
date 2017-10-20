9df08f8cbb732fa711ac25d2d590cce4cc5e4cf5
Mark Busenitz <mbusenitz@gmail.com>
2017-10-19 20:31:53 -0700

    Uni test (#3)
    * fix prima definition (#2)
    
    * Uni test (#3)
    
    * test doc hook
    
    * test svg
    
    * clang-format -i -style=WebKit *.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-#include <tmp/src/test.h>
 #include "matrix_test.h"
+#include <tmp/src/test.h>
 
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
     using namespace om636;
 
-	run_math_test<void>();
+    run_math_test<void>();
 
-	run_matrix_test();
-	return 0;
+    run_matrix_test();
+    return 0;
 }
